1fb75d26d1ad9f4cc9b9a65b756af9b3fdfa85a8
ZachErnesti <55199645+ZachErnesti@users.noreply.github.com>
2019-10-09 10:07:58 -0500

    Added query 44

--- Queries 1-53.sql
+++ Queries 1-53.sql
@@ -1,4 +1,17 @@
 
+
+/* *** SQL-Query-CH03-44 *** */ 
+SELECT FirstName, LastName 
+FROM EMPLOYEE 
+WHERE EmployeeNumber IN 
+(SELECT DISTINCT EmployeeNumber 
+FROM ASSIGNMENT 
+WHERE HoursWorked > 40 
+AND ProjectID IN 
+(SELECT ProjectID 
+FROM PROJECT 
+WHERE Department = 'Accounting'));
+
 /* *** SQL-Query-CH03-45 *** */ 
 SELECT FirstName, LastName, ProjectID, HoursWorked 
 FROM EMPLOYEE, ASSIGNMENT;
