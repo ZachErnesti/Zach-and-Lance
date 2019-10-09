113bb683be6f7df756b8a54047c5a84206660cca
ZachErnesti <55199645+ZachErnesti@users.noreply.github.com>
2019-10-09 10:01:23 -0500

    Added query 53

--- Queries 1-53.sql
+++ Queries 1-53.sql
@@ -1,1 +1,6 @@
 
+
+/* *** SQL-Query-CH03-53 *** */ 
+SELECT ProjectName, FirstName, LastName, HoursWorked 
+FROM EMPLOYEE AS E JOIN ASSIGNMENT AS A ON E.EmployeeNumber = A.EmployeeNumber JOIN PROJECT AS P ON A.ProjectID = P.ProjectID 
+ORDER BY P.ProjectID, A.EmployeeNumber;
